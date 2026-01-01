60f98006eb3386ad134d085e482ca436aaeb45d5
Georgekutty Jose <georgekuttyjose84@gmail.com>
2026-01-01 07:22:40 +0530

    feat: add environment configuration and loader

--- bootstrap/prepend.inc.php
+++ bootstrap/prepend.inc.php
@@ -3,3 +3,18 @@
 declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
+
+$envFile = __DIR__ . '/../.env';
+
+if (file_exists($envFile)) {
+    $lines = file($envFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($lines as $line) {
+        if (str_starts_with(trim($line), '#')) {
+            continue;
+        }
+        [$key, $value] = explode('=', $line, 2);
+        $_ENV[$key] = $value;
+        putenv("$key=$value");
+    }
+}
+
